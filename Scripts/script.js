01757fafd102a110e55a5c8b25d40ad1c54c8d05
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-09-09 14:58:48 +0100

    Update script.js

---
+++ Scripts/script.js
@@ -0,0 +1,7 @@
+/*BMI weight calculator
+*User enters height and weight if incorrect input, error message shows.
+*If input is correct BMI is calculated and BMI is shown using DOM-manipulation.
+*Needs way to combine feet and inches and stone and pounds. 
+*Then function to calculate BMI. 
+*Way to change height to cm and back and weight to kg and back.
+*/
