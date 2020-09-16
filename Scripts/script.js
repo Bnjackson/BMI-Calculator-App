afc68f2fbc707679d245d8280a8459e48bf0a25a
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-09-16 16:15:44 +0100

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -6,4 +6,20 @@
 *Way to change height to cm and back and weight to kg and back.
 */
 
+let submitButton = document.getElementById('button-submit');
+submitButton.addEventListener('click', calculateBMI);
+
+function calculateBMI (ev) { //ev refers to the actual click event itself.
+    ev.preventDefault(); //prevents form submitting
+    ev.stopPropagation(); //prevents bubbling
+    let feet = document.getElementById('height-feet-input').value;
+    let inches = document.getElementById('height-inches-input').value;
+    let stone = document.getElementById('weight-stone-input').value;
+    let pounds = document.getElementById('weight-pounds-input').value;
+    let height, weight, BMI;
+    height = (feet * 12) + Number(inches);
+    weight = (stone * 14) + Number(pounds);
+    BMI = weight/(height^2) * 703;
+    console.log(height, weight, BMI);
+}
 
