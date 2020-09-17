e769ffcc05db3f1d9977f5f3e6254a2d73e8cc90
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-09-17 02:27:38 +0100

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,19 +7,46 @@
 */
 
 let submitButton = document.getElementById('button-submit');
-submitButton.addEventListener('click', calculateBMI);
+submitButton.addEventListener('click', calculateHeightWeight);
 
-function calculateBMI (ev) { //ev refers to the actual click event itself.
+function calculateHeightWeight (ev) { //ev refers to the actual click event itself.
     ev.preventDefault(); //prevents form submitting
     ev.stopPropagation(); //prevents bubbling
     let feet = document.getElementById('height-feet-input').value;
     let inches = document.getElementById('height-inches-input').value;
     let stone = document.getElementById('weight-stone-input').value;
     let pounds = document.getElementById('weight-pounds-input').value;
-    let height, weight, BMI;
-    height = (feet * 12) + Number(inches);
-    weight = (stone * 14) + Number(pounds);
-    BMI = weight/(height^2) * 703;
-    console.log(height, weight, BMI);
+    let height = (feet * 12) + Number(inches);
+    let weight = (stone * 14) + Number(pounds);
+    function calculateBMI () {
+        if (height > 0 && weight > 0) {
+            let BMI = (weight /(height*height)) * 703.06957964;
+            BMI = BMI.toFixed(1);
+            displayBMI();
+            function displayBMI () {
+                let resultBMI = document.getElementById('BMI-result');
+                resultBMI.style.display = "block";
+                let numberBMI = document.getElementById('BMI-number');
+                numberBMI.textContent = BMI;
+                let weightRange = document.getElementById('weight-range');
+                if (BMI < 18.5) {
+                    weightRange.textContent = 'Underweight';
+                }
+                else if (BMI >= 18.5 && BMI <= 24.9) {
+                    weightRange.textContent = 'Normal or healthy weight';
+                }
+                else if (BMI >= 25 && BMI <= 29.9) {
+                    weightRange.textContent = 'Overweight';
+                }
+                else if (BMI >= 30) {
+                    weightRange.textContent = 'Obese';
+                }
+            }
+        }
+        else {
+
+        }
+    }
+    calculateBMI();
 }
 
